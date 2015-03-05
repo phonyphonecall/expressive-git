0e99892bfc5e8f71c2c05bccdcf8903b8c5c0cf7
Scott Hendrickson <phonyphonecall@aol.com>
2015-03-05 11:30:38 -0600

    added basic dependency installation :postbox:

--- install.sh
+++ install.sh
@@ -1,6 +1,18 @@
 #!/bin/sh
+brew -v || {
+    echo "Homebrew is required for expressive git!";
+    exit -1;
+}
+brew list randomize-lines || {
+    brew install randomize-lines || {
+        echo "unable to install required package randomize-lines"
+        exit -1;
+    }
+}
+
 mkdir -p /usr/local/share/emoji
 cp emojis_sorted /usr/local/share/emoji/
 cp commit-msg /usr/local/share/emoji/
 cp activate-expression /usr/local/share/emoji/
 ln activate-expression /usr/bin
+
