7c180d54a82541f4238016832fb087007ae483e2
Scott Hendrickson <phonyphonecall@aol.com>
2015-03-05 11:16:41 -0600

    add bin linking :five:

--- install.sh
+++ install.sh
@@ -2,4 +2,5 @@
 mkdir -p /usr/local/share/emoji
 cp emojis_sorted /usr/local/share/emoji/
 cp commit-msg /usr/local/share/emoji/
-
+cp activate-expression /usr/local/share/emoji/
+ln activate-expression /usr/bin
