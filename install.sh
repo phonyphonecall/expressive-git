9157b71f405de9f58a1620825d3845f62e2ed3ce
Scott Hendrickson <phonyphonecall@aol.com>
2015-03-08 14:37:28 -0500

    :black_large_square: replace rl with python script

--- install.sh
+++ install.sh
@@ -1,15 +1,4 @@
 #!/bin/sh
-brew -v || {
-    echo "Homebrew is required for expressive git!";
-    exit -1;
-}
-brew list randomize-lines || {
-    brew install randomize-lines || {
-        echo "unable to install required package randomize-lines"
-        exit -1;
-    }
-}
-
 mkdir -p /usr/local/share/emoji
 cp emojis_sorted /usr/local/share/emoji/
 cp commit-msg /usr/local/share/emoji/
